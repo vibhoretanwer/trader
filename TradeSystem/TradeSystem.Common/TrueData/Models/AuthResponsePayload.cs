6011f2cb25e39217d52f646eec56348653ff8ba6
Vibhore Tanwer <vibhore.tanwer@nagarro.com>
2020-05-14 22:21:29 +0530

    Added basic infra for TrueData API

---
+++ TradeSystem/TradeSystem.Common/TrueData/Models/AuthResponsePayload.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradeSystem.Common.TrueData.Models
+{
+    public class AuthResponsePayload
+    {
+        public string authorization_code { get; set; }
+    }
+}
